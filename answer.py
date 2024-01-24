ea5f7df0216f3f6feb91942757b3936c6058fab6
“Gangadhar <gsirigir1@stevens.edu>
2024-01-24 15:56:00 -0500

    Answer for homwork 0 is uploded

--- answer.py
+++ answer.py
@@ -2,16 +2,17 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+     THIS FUNCTION IS USED TO PRINT HELLO WORLD 
     """
 
     # Write a comment; comments are very useful to other programmers.
-
+    # Print function is used to display Hello world on screen
     # Write a Python statement to print Hello, World!
+    print("Hello, World!")
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
